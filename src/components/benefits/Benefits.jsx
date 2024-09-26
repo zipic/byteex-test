29c6767698ca1d7ca0287124ef52cde12de142fb
zipic <zpc23041988@gmail.com>
2024-09-26 18:45:08 +0300

    add comment

---
+++ src/components/benefits/Benefits.jsx
@@ -0,0 +1,12 @@
+import "./style.scss";
+
+export const Benefits = () => {
+  return (
+    <div className="benefits">
+      <p className="text">as seen in</p>
+      <section className="benefits-partners"></section>
+    </div>
+  );
+};
+
+export default Benefits;
