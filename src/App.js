30473cadb28d2f35bceae4a9378c126dd27a5695
zipic <zpc23041988@gmail.com>
2024-09-26 12:19:50 +0300

    Initial commit: add first block

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/header/Header";
+import Hero from "./components/hero/Hero";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import "./style.scss";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <p className="header-text">
+        CONSCIOUSLY MADE BUTTER SOFT STAPLES FOR EVERY DAY (OR NIGHT) | FREE
+        SHIPPING on orders {">"} $200 | easy 45 day return window.
+      </p>
+    </header>
+  );
+};
+
+export default Header;
